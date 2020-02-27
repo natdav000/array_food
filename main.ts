303a7c028cecfcd31e30e522c948a0acd75633fd
natdav000 <58708585+natdav000@users.noreply.github.com>
2020-02-27 13:24:04 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+
+let food1 = "pizza "
+let food2 = "nachos "
+let food3 = "chili cheese fries "
+let food4 = "buffalo wings "
+let food5 = "burritos "
+let listFoods = ["pizza, ",  "nachos ", "chili cheese fries, ", "buffalo wings ", "burrtios "]
+game.splash("this is my list of favorite foods: " + listFoods[0] + listFoods[2] + listFoods[4])
